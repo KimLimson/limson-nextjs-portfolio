416e65c77b3aae77a59a03500585261031cc0e76
Ivan Limson <iv4n.d3v@gmail.com>
2025-08-14 13:13:01 +0800

    fix: ci deployment issue for build step

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -10,6 +11,14 @@ const nextConfig: NextConfig = {
   },
   basePath: isProd ? '/limson-nextjs-portfolio' : '',
   assetPrefix: isProd ? '/limson-nextjs-portfolio/' : '',
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve?.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
